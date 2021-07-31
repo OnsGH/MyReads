403eab674d2a7914af5f791867e9f9065ade4d17
Ons Ghariani <ons@Onss-MacBook-Pro.local>
2021-07-31 01:27:27 -0700

    Format file

--- src/App.js
+++ src/App.js
@@ -1,44 +1,34 @@
-import React from 'react'
+import React from "react";
 
-import {Route,Link, Switch} from 'react-router-dom';
-import ListBooks from './components/ListBooks'
-import SearchBooks from './components/SearchBooks'
+import { Route, Link, Switch } from "react-router-dom";
+import ListBooks from "./components/ListBooks";
+import SearchBooks from "./components/SearchBooks";
 
-import './App.css'
+import "./App.css";
 
 class BooksApp extends React.Component {
+ render() {
+  return (
+   <div className="app">
+    <Switch>
+     <Route
+      exact
+      path="/"
+      render={() => (
+       <div>
+        <ListBooks />
+        <div className="open-search">
+         <Link to="/SearchBooks">Search</Link>
+        </div>
+       </div>
+      )}
+     />
 
-
-render() {
- 
-    return (
-      <div className="app">
-        <Switch>
-            <Route exact path='/' 
-                render={() => ( 
-                 
-               <div>
-                
-                 <ListBooks  />
-                <div className="open-search">
-                  <Link to="/SearchBooks">Search</Link> 
-                </div>
-
-                
-             </div>
-               )
-               }
-          /> 
-       
-       
-       <Route exact path="/SearchBooks" component={SearchBooks}  />
-       
-        
-      </Switch>
-      </div>
-    )
-  }
+     <Route exact path="/SearchBooks" component={SearchBooks} />
+    </Switch>
+   </div>
+  );
+ }
 }
 
-
-export default BooksApp
+export default BooksApp;
