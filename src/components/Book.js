945b4c5ddd7a2c00a2a373d57bc31fdb54aaa37d
Ons Ghariani <ons@Onss-MacBook-Pro.local>
2021-07-31 01:25:21 -0700

    Fix bugs and implement checkBookCover

--- src/components/Book.js
+++ src/components/Book.js
@@ -4,44 +4,23 @@ import BookShelfChanger from './BookShelfChanger'
 import * as BooksAPI from '../BooksAPI'
 class Book extends Component{
     
-   // console.log(props.shelf)
+ 
    constructor(props) {
     super(props)
-
-    console.log('PROPS const', this.props)
-    this.state = {
-        shelf:'',
-        listOfBooks:[],
-        
-    } 
-   this.handleChange = this.handleChange.bind(this)
-   this.checkBookCover = this.checkBookCover.bind(this)
-  // this.getAllBooks = this.getAllBooks.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+    this.checkBookCover = this.checkBookCover.bind(this)
+  
 }
 handleChange(event){
-    const {name,value} = event.target
    
-    this.setState({
-      [name]:value
-     
-     })
-    // update
-    console.log('Name ', this.state.shelf)
-   /* 
-    BooksAPI.update(this.props.book,this.state.shelf).
-    then(
-        () => {
-        this.getAllBooks();
+    // Update books
+    const {value} = event.target
+    BooksAPI.update(this.props.book,value).then(() => {
+        this.props.getAllBooks();
     })
 }
 
-getAllBooks(){
 
-    BooksAPI.getAll().then((response) => {
-        this.setState({ listOfBooks: response });
-    });
-*/
-}
 checkBookCover(book){
  const bookCover = book.imageLinks && book.imageLinks.thumbnail
     ? book.imageLinks.thumbnail
@@ -52,12 +31,12 @@ return bookCover
     render(){
        
     return (
-        // map update
+        
        
-     <div class='book' >
+     <div className='book' >
         <div className="book-top">
           <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${this.checkBookCover(this.props.book)})`  }}></div>
-          <BookShelfChanger handleChange={this.handleChange} shelf={this.state.shelf} defaultValue={this.props.defaultValue} />
+          <BookShelfChanger handleChange={this.handleChange} defaultValue={this.props.defaultValue} />
         </div>
     
        <div className="book-title">{this.props.book.title}</div>
