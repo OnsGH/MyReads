9d4acccf558488a02dc3eb29b315c47db99d8322
Ons Ghariani <ons@Onss-MacBook-Pro.local>
2021-07-31 01:26:23 -0700

    Bugs fix and format file

--- src/components/BookShelfChanger.js
+++ src/components/BookShelfChanger.js
@@ -1,22 +1,18 @@
-import React from 'react'
+import React from "react";
 
-function BookshelfChanger(props){
-    return (
-      
-<div className="book-shelf-changer">
-{ console.log('Props changer', props.defaultValue)}
-            <select value={props.shelf} 
-                    name="shelf" 
-                    onChange={props.handleChange} defaultValue={props.defaultValue}>
-              <option value="move" disabled>Move to...</option>
-              <option value="currentlyReading">Currently Reading</option>
-              <option value="wantToRead">Want to Read</option>
-              <option value="read">Read</option>
-              <option value="none">None</option>
-            </select>
-          </div>
-          
-    )
-        
-        }
-    export default BookshelfChanger
+function BookshelfChanger(props) {
+ return (
+  <div className="book-shelf-changer">
+   <select value={props.defaultValue} name="shelf" onChange={props.handleChange}>
+    <option value="move" disabled>
+     Move to...
+    </option>
+    <option value="currentlyReading">Currently Reading</option>
+    <option value="wantToRead">Want to Read</option>
+    <option value="read">Read</option>
+    <option value="none">None</option>
+   </select>
+  </div>
+ );
+}
+export default BookshelfChanger;
