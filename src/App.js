e380611ddb0c7a3ecf758fa585cf55863f3475d7
Ons Ghariani <ons@Onss-MacBook-Pro.local>
2021-08-02 01:43:22 -0700

    Moved listofbooks state, componentDidMount method and getAllBooks method from ListBooks class to App.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,27 @@ import React from "react";
 import { Route, Link, Switch } from "react-router-dom";
 import ListBooks from "./components/ListBooks";
 import SearchBooks from "./components/SearchBooks";
-
+import * as BooksAPI from "./BooksAPI";
 import "./App.css";
 
 class BooksApp extends React.Component {
+ constructor() {
+  super();
+  this.state = {
+   listOfBooks: [],
+  };
+ }
+
+ getAllBooks = () => {
+  BooksAPI.getAll().then((response) => {
+   this.setState({ listOfBooks: response });
+  });
+ };
+
+ componentDidMount() {
+  this.getAllBooks();
+ }
+
  render() {
   return (
    <div className="app">
@@ -16,15 +33,23 @@ class BooksApp extends React.Component {
       path="/"
       render={() => (
        <div>
-        <ListBooks />
+        <ListBooks listOfBooks={this.state.listOfBooks} getAllBooks={this.getAllBooks} />
         <div className="open-search">
-         <Link to="/SearchBooks">Search</Link>
+         <Link to="/search">Search</Link>
         </div>
        </div>
       )}
      />
 
-     <Route exact path="/SearchBooks" component={SearchBooks} />
+     <Route
+      exact
+      path="/search"
+      render={() => (
+       <div>
+        <SearchBooks listOfBooks={this.state.listOfBooks} getAllBooks={this.getAllBooks} />
+       </div>
+      )}
+     />
     </Switch>
    </div>
   );
